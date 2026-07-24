5b0f8c6fb0e9c5ab1b509e22e5fb2878b3214dbd
Anna Valentina <lopezbotarda@gmail.com>
2026-07-24 17:44:48 +0100

    Add files via upload

---
+++ portafolioTest2/script.js
@@ -0,0 +1,27 @@
+ // window.addEventListener("load", () => {
+    // const continuar = window.confirm(
+  //   "⚠️ Alerta!\n\nEste site pode conter pouca informação.\n\nDeseja continuar?"
+    );
+
+  //  if (continuar) {
+      //  console.log("O utilizador escolheu continuar.");
+  //  } else {
+    //     window.location.href = "https://www.google.com";
+        // Ou:
+        // window.close(); // Apenas funciona em janelas abertas por JavaScript
+    }
+});
+
+//  window.onload = function () {
+
+//    let resposta = confirm(
+  //      "⚠️ Alerta!\n\nEste site pode conter pouca informação.\n\nDeseja continuar?"
+    );
+
+ //   if (resposta) {
+ //       alert("Bem-vindo ao meu portfólio!");
+    } else {
+ //       window.location.href = "about:blank";
+    }
+
+};
